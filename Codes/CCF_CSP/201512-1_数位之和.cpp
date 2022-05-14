c9bbd44c0efe633d555d4cb5bb734c6af732b8b8
Chenghe Hu <st4r94z3r@protonmail.com>
2022-05-14 21:09:28 +0800

    add new branch

--- Codes/CCF_CSP/201512-1_数位之和.cpp
+++
@@ -1,7 +0,0 @@
-#include<cstdio>
-int main(){
-	char c = getchar(); long long total = 0;
-	while (c != '\n') {total += c - '0'; c = getchar();}
-	printf("%lld\n", total);
-	return 0; 
-}
--- Codes/CCF_CSP/201709-1_打酱油.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-int N;
-int main()
-{
-	std::cin >> N; N /= 10;
-	std::cout << N + 2 * (N / 5) + (N % 5) / 3; 
-	return 0;
-}
